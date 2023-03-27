ae529a97d1f209e7a875d4e6b05a042275c3bf88
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-03-27 11:15:03 -0300

    First commit

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double xA, xB, xC, yA, yB, yC;
+
+        System.out.println("Entre com as medidas do Triangulo X: ");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+
+        System.out.println("Entre com as medidas do Triangulo Y: ");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        double p = (xA + xB + xC) / 2;
+        double areaX = Math.sqrt(p *(p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2;
+        double areaY = Math.sqrt(p *(p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("Triangulo X: %.4f%n", areaX);
+        System.out.printf("Triangulo Y: %.4f%n", areaY);
+
+        if(areaX > areaY){
+            System.out.println("Area maior: X");
+        }else{
+            System.out.println("Area maior: Y");
+        }
+
+        sc.close();
+    }
+}
